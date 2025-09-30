03d361e84f8ad5362fbdbd6715eff386c0ce45ec
André Alves Correia <andre2013aguabranca@gmail.com>
2025-09-30 16:52:32 -0300

    add lazy loading to product images

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,18 +1,23 @@
-import type { ComponentProps } from 'react';
-import { SecondaryButton } from '../SecondaryButton';
+import type { ComponentProps } from 'react'
+import { SecondaryButton } from '../SecondaryButton'
 import './style.css'
 type DivProps = ComponentProps<'div'>
 
 export interface ProductCardProps {
-  img: string;
-  title: string;
-  description: string;
+  img: string
+  title: string
+  description: string
 }
 
-export default function ProductCard({ img, title, description, ...divProps }: ProductCardProps & DivProps) {
+export default function ProductCard({
+  img,
+  title,
+  description,
+  ...divProps
+}: ProductCardProps & DivProps) {
   return (
     <div className="product-card" {...divProps}>
-      <img src={img} alt="Product" />
+      <img src={img} loading="lazy" alt="Product" />
       <p>{title}</p>
       <small>{description}</small>
       <SecondaryButton>Eu quero</SecondaryButton>
